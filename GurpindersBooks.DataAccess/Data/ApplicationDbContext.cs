08d6f267c1c4b178075103d02be65530bd8a4280
W0828759 <W0828759@myscc.ca>
2023-11-09 18:59:59 -0500

    add category database

--- GurpindersBooks.DataAccess/Data/ApplicationDbContext.cs
+++ GurpindersBooks.DataAccess/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using GurpindersBooks.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -12,5 +13,7 @@ namespace GurpinderBookStore.DataAccess.Data
             : base(options)
         {
         }
+
+        public DbSet<Category> Categories { get; set; }
     }
 }
