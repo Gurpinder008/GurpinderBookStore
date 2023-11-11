f3acbcc49a1590945ae6e82ae36145e24e2492f7
W0828759 <W0828759@myscc.ca>
2023-11-11 13:37:19 -0500

    table

--- GurpinderBookStore/Areas/Admin/Controllers/CategoryController.cs
+++ GurpinderBookStore/Areas/Admin/Controllers/CategoryController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 
 using GurpindersBooks.DataAccess.Repository.IRepository;
+using GurpindersBooks.Models;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -21,6 +22,22 @@ namespace GurpinderBookStore.Areas.Admin.Controllers
         }
         public IActionResult Index()
         {
+            return View();
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            Category category = new Category();
+            if(id == null)
+            {
+                return View(category);
+            }
+
+            category = _unitOfWork.Category.Get(id.GetValueOrDefault());
+            if(category == null)
+            {
+                return NotFound();
+            }
             return View();
         }
 
