7a844c2d1931eab7ddea480d1f4be0879f1cf9d0
W0828759 <W0828759@myscc.ca>
2023-11-20 13:45:37 -0500

    add product class, add migration

--- GurpindersBooks.DataAccess/Migrations/20231120180052_addValidationToProduct.Designer.cs
+++ GurpindersBooks.DataAccess/Migrations/20231120180052_addValidationToProduct.Designer.cs
@@ -4,14 +4,16 @@ using GurpinderBookStore.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GurpindersBooks.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231120180052_addValidationToProduct")]
+    partial class addValidationToProduct
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ GurpindersBooks.Models/CoverType.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GurpindersBooks.Models
+{
+    public class CoverType
+    {
+        [Key]
+        public int CoverTypeId { get; set; }
+    }
+}
