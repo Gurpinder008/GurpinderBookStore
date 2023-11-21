6b5e0760a251d449baf6c2ecda720f3a33020033
W0828759 <W0828759@myscc.ca>
2023-11-20 19:39:16 -0500

    add covertype and icovertype repos to the project

---
+++ GurpindersBooks.DataAccess/Repository/CoverTypeRepository.cs
@@ -0,0 +1,34 @@
+﻿using GurpindersBooks.DataAccess.Repository.IRepository;
+using GurpindersBooks.Models;
+using GurpinderBookStore.DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace GurpindersBooks.DataAccess.Repository
+{
+    public class CoverTypeRepository : Repository<Category>, ICategoryRepository
+    {
+
+        private readonly ApplicationDbContext _db;
+        public CoverTypeRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Category category)
+        {
+            var objFromDb = _db.Categories.FirstOrDefault(s => s.Id == category.Id);
+            if (objFromDb != null)
+            {
+                objFromDb.Name = category.Name;
+                // _db.SaveChanges();
+            }
+        }
+
+    }
+}
+    
+
--- GurpindersBooks.Models/CoverType.cs
+++ GurpindersBooks.Models/CoverType.cs
@@ -1,10 +1,19 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace GurpindersBooks.Models
 {
     public class CoverType
     {
         [Key]
-        public int CoverTypeId { get; set; }
+        public int Id { get; set; }
+        [Display(Name = "CoverType Name")]
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
     }
 }
