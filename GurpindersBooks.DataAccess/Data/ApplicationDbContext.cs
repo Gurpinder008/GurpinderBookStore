0c40d07e69369ba04ea3e4ba0b0e523cc1a824be
W0828759 <W0828759@myscc.ca>
2023-11-28 13:11:35 -0500

    Review applicationdb - change namespace

--- GurpindersBooks.DataAccess/Data/ApplicationDbContext.cs
+++ GurpindersBooks.DataAccess/Data/ApplicationDbContext.cs
@@ -15,8 +15,8 @@ namespace GurpinderBookStore.DataAccess.Data
         }
 
         public DbSet<Category> Categories { get; set; }
-
         public DbSet<CoverType> CoverTypes { get; set; }
+
         public DbSet<Product> Products { get; set; }
     }
 }
